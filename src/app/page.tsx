2d67ce46df608c2a829bafb04fe74e9024af24b2
anjanah07 <thanjana7@gmail.com>
2024-10-13 12:17:29 +0530

    add global styles and mock urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function HomePage() {
+  const mockURLS = [
+    "https://utfs.io/f/N1tfczHUlMFvXkZW0ZjRU1q5FaIhcAk6ibC8QtyEWKjd3G7s",
+    "https://utfs.io/f/N1tfczHUlMFvnNBKphFDsRteGSB8Xvl1nMKkYd4jUOz3AN70",
+    "https://utfs.io/f/N1tfczHUlMFv3I08iSb9nhLwoYUj3AkurVKslHiqJ42mpO6d",
+  ];
+  const mockImages = mockURLS.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+  //https://utfs.io/f/N1tfczHUlMFvXkZW0ZjRU1q5FaIhcAk6ibC8QtyEWKjd3G7s
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery
+    <main>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id}>
+            <img src={image.url} alt="image" width={400} height={200} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
