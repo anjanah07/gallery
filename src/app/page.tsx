87734b2ea4d3a8bd37391ec541c7a4e4ff4a37c6
anjanah07 <thanjana7@gmail.com>
2024-10-29 16:23:05 +0530

    add : desc order by

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 export default async function HomePage() {
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main>
       <div className="grid w-auto grid-cols-3 gap-2">
