bd5ec2f24d5b92dc38ec68df45a10f1e96f43e7c
anjanah07 <thanjana7@gmail.com>
2024-11-06 11:11:54 +0530

    add: new img folder

---
+++ src/app/img/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotoModal = ({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) => {
+  return <div>{photoId}</div>;
+};
+
+export default PhotoModal;
