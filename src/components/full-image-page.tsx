f361c63a26d5fcc94677c41933e840b2b26fe572
anjanah07 <thanjana7@gmail.com>
2024-11-08 23:10:25 +0530

    add border and center the img

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -6,7 +6,7 @@ const FullPageImageViewPhoto = async (props: { id: number }) => {
   if (!image || image instanceof Error) return <div>Error loading image</div>;
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex-shrink">
+      <div className="flex flex-shrink items-center justify-center">
         <img
           src={image.url}
           alt={image.name}
@@ -14,7 +14,7 @@ const FullPageImageViewPhoto = async (props: { id: number }) => {
           // className="w-full"
         />
       </div>
-      <div className="flex w-48 flex-shrink-0">
+      <div className="flex w-48 flex-shrink-0 border-l">
         <div className="text-xl font-bold">{image.name}</div>
       </div>
     </div>
