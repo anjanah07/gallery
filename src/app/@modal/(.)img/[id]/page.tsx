96f0d7528166d6c95525f64ed9ddf869692d4a7f
anjanah07 <thanjana7@gmail.com>
2024-11-08 10:10:36 +0530

    add:query to fetch img; render in ui

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import { getImage } from "~/server/queries";
 
-const PhotoModal = ({
+const PhotoModal = async ({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) => {
-  return <div>{photoId}</div>;
+  const photoIdasNum = Number(photoId);
+  if (Number.isNaN(photoIdasNum)) return <div>Invalid ID</div>;
+  const image = await getImage(photoIdasNum);
+  if (!image || image instanceof Error) return <div>Error loading image</div>;
+  return (
+    <div>
+      <Image
+        src={image.url}
+        alt={image.name}
+        height={800}
+        width={800}
+        className="w-full"
+      />
+    </div>
+  );
 };
 
 export default PhotoModal;
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -11,3 +11,13 @@ export const getMyImages = async () => {
   });
   return images;
 };
+export const getImage = async (id: number) => {
+  const user = await auth();
+  if (!user.userId) return new Error("Unauthorized");
+  const image = await db.query.images.findFirst({
+    where: (modal, { eq }) => eq(modal.id, id),
+  });
+  if (!image) return new Error("Image not found");
+  if (image.userId !== user.userId) return new Error("Incorrect user");
+  return image;
+};
