6cb6c5a95d82f1a6f596730c3e65ba006b5dfccf
anjanah07 <thanjana7@gmail.com>
2024-11-09 18:19:25 +0530

    add: user full name

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,21 +1,26 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import React from "react";
 import { getImage } from "~/server/queries";
 
 const FullPageImageViewPhoto = async (props: { id: number }) => {
   const image = await getImage(props.id);
   if (!image || image instanceof Error) return <div>Error loading image</div>;
+  const user = await clerkClient.call(this);
+  const userInfo = await user.users.getUser(String(image.userId));
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
+      <div className="flex w-1/2 flex-shrink">
         <img
           src={image.url}
           alt={image.name}
           className="flex-shirnk object-contain"
-          // className="w-full"
         />
       </div>
-      <div className="flex w-48 flex-shrink-0 border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="itmems-center flex w-1/2 flex-shrink-0 justify-center gap-2 border-l">
+        <div className="p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col">
+          <span>Uploaded By :{userInfo.fullName}</span>
+        </div>
       </div>
     </div>
   );
